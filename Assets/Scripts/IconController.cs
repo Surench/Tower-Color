c3f2efa72d1abd41615f96de757cf6a732927eeb
surench <suren.melkonyan@yahoo.com>
2020-07-03 00:27:07 +0400

    Adding shooter system

---
+++ Assets/Scripts/IconController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconController : MonoBehaviour
+{
+
+	[HeaderAttribute("Animation Curve")]
+	public AnimationCurve animationCurve;
+
+	private Vector3 initialScale;
+	private Vector3 finalScale;
+	private float graphValue;
+
+	private void Awake()
+	{
+		initialScale = transform.localScale;
+		finalScale = Vector3.one;
+		animationCurve.postWrapMode = WrapMode.PingPong;
+	}
+
+	private void Start()
+	{
+	}
+
+	public void InitImg()
+	{
+		StartCoroutine(UpdateImg());
+	}
+
+	IEnumerator UpdateImg()
+	{
+		float startTime = Time.time;
+		float duration = 1.5f;
+		float t = 0;
+
+		while (t<1)
+		{
+			t = (Time.time - startTime) / duration;
+
+			graphValue = animationCurve.Evaluate(t);
+			transform.localScale = finalScale * graphValue;
+
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	private void Update()
+	{
+		//graphValue = animationCurve.Evaluate(Time.time);
+		//transform.localScale = finalScale * graphValue;
+	}
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+	[SerializeField] Rigidbody rb;
+	[SerializeField] GameObject bullet;
+
+	public LayerMask layer;
+	public GameObject cursor;
+
+	public Vector3 newPos;
+	private void Update()
+	{
+		InitProjectile();
+	}
+
+	void InitProjectile()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		Debug.DrawRay(ray.origin, ray.direction * 30, Color.red);
+		if (Physics.Raycast(ray, out hit,100 ))
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				Vector3 Vo = CalcualateVelocity(hit.point, transform.position , 0.4f);
+
+				GameObject ob = Instantiate(bullet, transform.position, Quaternion.identity);
+				Rigidbody rb = ob.GetComponent<Rigidbody>();
+				rb.velocity = Vo;
+			}	
+		
+		}
+	}
+
+	Vector3 CalcualateVelocity (Vector3 target, Vector3 origin , float time)
+	{
+		//define the distance x - y
+		Vector3 distance = target - origin;
+		Vector3 distancXZ = distance;
+		distancXZ.y = 0;
+
+		//new float represent distanc
+		float Sy = distance.y;
+		float Sxz = distance.magnitude;
+
+		float Vxz = Sxz / time;
+		float Vy = Sy / time + 0.5f * Mathf.Abs( Physics.gravity.y) * time;
+
+		Vector3 result = distancXZ.normalized;
+		result *= Vxz;
+		result.y = Vy;
+
+		return result;
+	}
+}
